b7d751e85c670f82b4b97c0780c0f0c6c6dc70f2
Srinja <63492855+Srinja@users.noreply.github.com>
2026-03-03 12:24:53 +0530

    Update App.java with App2 main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins + Maven!");
-        App2.sayHello();
+      
     }
 
 }
@@ -12,8 +12,11 @@ class App2 {
     public static void sayHello() {
         System.out.println("Hello from App2 class");
     }
-
+    public static void main(String[] args) {
+        sayHello();      
+    }
 }
+
 
 
 
